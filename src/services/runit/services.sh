1483d611c69abf79a780e40cd20b19049e82c0ff
Max <teratoscincus@hotmail.com>
2024-05-13 19:31:05 +0200

    feat(services): setup services per runit

---
+++ src/services/runit/services.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=$(dirname "$0")/../../..
+
+source "$ROOT_DIR"/src/helpers/output_labels.sh
+
+enable_service() {
+    # Takes an arbitrary number of string arguments and enables their service if they
+    # are installed.
+    ORIGIN="/etc/sv"
+    SERVICES="/var/service"
+    for service in "${@}"; do
+        if [[ -d "$ORIGIN/$service/" ]]; then
+            sudo ln -s "$ORIGIN/$service/" "$SERVICES/"
+        else
+            warn "Failed to enable $service"
+            warn "  Not found in $ORIGIN/$service"
+        fi
+    done
+}
+
+disable_service() {
+    # Takes an arbitrary number of string arguments and disables their service if they
+    # are enabled.
+    SERVICES="/var/service"
+    for service in "${@}"; do
+        if [[ -d "$SERVICES/$service" ]]; then
+            sudo rm "$SERVICES/$service"
+        fi
+    done
+}
+
+# Utility services
+
+# Dbus
+enable_service "dbus"
+
+# Udev
+enable_service "udevd"
+
+# NetworkManager
+disable_service "dhcpcd" "wpa_supplicant" "wicd"
+enable_service "NetworkManager"
+
+# Elogind
+disable_service "acpid"
+enable_service "elogind"
+
+# Syslog
+enable_service "socklog-unix" "nanoklogd"
+
+# Alsa
+enable_service "alsa"
+
+# Bluetooth
+enable_service "bluetoothd"
+
+# Printers
+enable_service "cupsd"
+
+# Dev tool services
+
+# Docker
+enable_service "containerd" "docker"
+
+# Clean up
+
+unset -v ok
+unset -f enable_service disable_service
