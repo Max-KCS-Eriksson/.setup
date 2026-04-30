78096cc0698d90a1f79e279400ce975ef73e27ca
Max Eriksson <teratoscincus@hotmail.com>
2026-04-30 13:32:58 +0200

    docs: proper README
    Remove usage documenting comments from source code.

--- src/extras/extra_setup.sh
+++ src/extras/extra_setup.sh
@@ -14,7 +14,6 @@ install_tool() {
 }
 
 # Setup installed tools
-# Place script in the `./setup` dir to setup tools handled by the package manager.
 for tool in "$HERE"/setup/*; do
     if [[ -f $tool ]]; then
         install_tool "$tool"
@@ -22,8 +21,6 @@ for tool in "$HERE"/setup/*; do
 done
 
 # Install extra tools
-# Place script in the `./tools` dir to install and setup tools that are not handled by
-# the package manager.
 for tool in "$HERE"/tools/*; do
     if [[ -f $tool ]]; then
         install_tool "$tool"
